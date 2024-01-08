02923d1cc52f88068ebf1312b786530b273e74f1
adelaide <adelaide.chanteux@epitech.eu>
2024-01-08 10:26:27 +0100

    Add joke API

--- electro-bob/pages/index.vue
+++ electro-bob/pages/index.vue
@@ -4,10 +4,27 @@
         <div class="buttonWrapper">
             <div class="base">
                 <h1>WELCOME</h1>
+                <h2> {{result}} </h2>
             </div>
         </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+const result = ref('');
+const url = 'https://v2.jokeapi.dev/joke/Any?format=txt&lang=fr&blacklistFlags=nsfw';
+const options = {
+	method: 'GET',
+};
+
+try {
+	const response = await fetch(url, options);
+	result.value = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+</script>
 
 <style>
 .buttonWrapper {
