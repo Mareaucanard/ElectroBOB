1ab4ab85a43245d590d6a5bfedbc0e0561b99248
root <thibault.mir@epitech.eu>
2024-01-15 09:32:26 +0100

    Add userId and token to resgister

---
+++ Eletro-BOB_API/Classes/returnedUser.cs
@@ -0,0 +1,11 @@
+namespace Eletro_BOB_API.Classes
+{
+    public class returnedUser
+    {
+        public int userId { get; set; }
+
+        public string jwt { get; set; }
+
+        public string username { get; set; }
+    }
+}
